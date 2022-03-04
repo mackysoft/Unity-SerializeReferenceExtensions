e7a8195f17c598290cc1b256cf01025ae2b4caa7
mackysoft <mackysoft0129@gmail.com>
2022-03-05 02:15:38 +0900

    Update ExampleEditor.cs

--- Assets/MackySoft/MackySoft.SerializeReferenceExtensions/Example/ExampleAssets/Scripts/Editor/ExampleEditor.cs
+++ Assets/MackySoft/MackySoft.SerializeReferenceExtensions/Example/ExampleAssets/Scripts/Editor/ExampleEditor.cs
@@ -2,10 +2,10 @@ using UnityEditor;
 
 namespace MackySoft.SerializeReferenceExtensions.Example.Editor {
 
-	[CustomEditor(typeof(Example))]
+	// Enabling the custom editor slowdown rendering performance.
+	//[CustomEditor(typeof(Example))]
 	public class ExampleEditor : UnityEditor.Editor {
 
-		// Enabling the custom editor slowdown rendering performance.
 		public override void OnInspectorGUI () {
 			base.OnInspectorGUI();
 		}
